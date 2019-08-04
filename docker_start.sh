756b1e449dd8aedbdac7ed576fe1258645c49ec3
Jidor Tang <tlqtangok@126.com>
2019-08-04 11:28:10 +0800

    - edit docker_start.sh at 20190804_1128

---
+++ docker_start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#service ssh start
+date 
+# export JD=JD_ENV_VAR_VALUE  # no meanings 
+echo "- successfully start this container"
+#bash -c "bash  /root/jd/t/ngrok/start_ngrok.sh &" &
+#/usr/sbin/sshd -D -e -f /etc/ssh/sshd_config
+
+echo "- start http server on 10205"
+perl /root/jd/t0/start_http_server.PL start &
+
+service ssh start &
+/bin/bash
