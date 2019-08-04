ce7081de5a9b43ad51d11bc3f99eea014443a5f3
Jidor Tang <tlqtangok@126.com>
2019-08-04 16:03:06 +0800

    - edit run_ub_ssh_uwsgi_http_version.sh at 20190804_1603

--- run_ub_ssh_uwsgi_http_version.sh
+++ run_ub_ssh_uwsgi_http_version.sh
@@ -1,5 +1,9 @@
 # for algo
-export VERSION=v2 && docker run  -dit -p 10203:22 -p 10204:10205 -v /root/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
+# export VERSION=v2 && docker run  -dit -p 10203:22 -p 10204:10205 -v /root/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
 
 # for jd p100
-# export VERSION=v2 && docker run  -dit -p 10204:22 -p 10205:10205 -v /mnt/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
+export VERSION=v2 && docker run  -dit -p 10204:22 -p 10205:10205 -v /mnt/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
+ 
+# docker save it
+# cd $t && export VERSION=v2 &&  time docker save -o ub_ssh_uwsgi_http_${VERSION}.tar ub_ssh_uwsgi_http:$VERSION  && time tar czf ub_ssh_uwsgi_http_$VERSION.tar.tar.gz ub_ssh_uwsgi_http_$VERSION.tar
+
