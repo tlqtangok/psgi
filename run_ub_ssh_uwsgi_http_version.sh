e09dc441874462a01e272d1555aaf165528017a7
Jidor Tang <tlqtangok@126.com>
2019-08-04 15:01:07 +0800

    enable algo

--- run_ub_ssh_uwsgi_http_version.sh
+++ run_ub_ssh_uwsgi_http_version.sh
@@ -1,5 +1,5 @@
 # for algo
-# export VERSION=v2 && docker run  -dit -p 10203:22 -p 10204:10205 -v /root/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
+export VERSION=v2 && docker run  -dit -p 10203:22 -p 10204:10205 -v /root/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
 
 # for jd p100
-export VERSION=v2 && docker run  -dit -p 10204:22 -p 10205:10205 -v /mnt/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
+# export VERSION=v2 && docker run  -dit -p 10204:22 -p 10205:10205 -v /mnt/jd/t/vt:/root/jd/t  --name ${VERSION}_0 -h ${VERSION}_0 ub_ssh_uwsgi_http:$VERSION
